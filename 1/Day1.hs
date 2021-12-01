87aabe00e4db35aeed15c674d331288e4cd81255
Quinten <qvdpost@gmail.com>
2021-12-01 12:23:37 +0100

    refactor

--- 1/Day1.hs
+++ 1/Day1.hs
@@ -1,11 +1,10 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Day1 where
 
 
-import Data.List
+import Data.List ()
 import Helper (exercise)
-import System.IO
-import Control.Monad
+import System.IO ()
+import Control.Monad ()
 
 readInt :: String -> Int
 readInt = read
@@ -13,23 +12,22 @@ readInt = read
 sucIncreasing :: [Int] -> Int
 sucIncreasing (a:b:ns) | b > a = 1 + sucIncreasing (b : ns)
                        | otherwise = sucIncreasing (b : ns)
-sucIncreasing [a, b] | b > a = 1
-                     | otherwise = 0
 sucIncreasing [_] = 0
 sucIncreasing [] = 0
 
-poolValues :: [Int] -> [Int]
-poolValues (a:b:c:ns) = (a + b + c) : poolValues (b:c:ns)
-poolValues [a, b, c] = [a + b + c]
-poolValues [a, b] = [a + b]
-poolValues [a] = [a]
-poolValues [] = []
+poolValues :: [Int] -> Int -> [Int]
+poolValues [] n = []
+poolValues ns n = sum (take n ns) : poolValues (tail ns) n
+
 
 _main :: IO ()
 _main = do
     putStrLn $ exercise 1 "Find number of increasing depths"
     contents <- readFile "data.txt"
-    let values = poolValues $ map readInt . words $ contents
 
-    print $ sucIncreasing values
+    let values = map readInt . words $ contents
+    print $ "Successive increases in depth: " ++ show (sucIncreasing values)
+
+    let values = poolValues (map readInt . words $ contents) 3
+    print $ "Successive increases in combined measurements: " ++ show (sucIncreasing values)
 
